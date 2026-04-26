0dea38e0d1b4508b7dca2d2f7381239d4fae83b4
rostyslav.bodnar-ip224@nung.edu.ua <rostyslav.bodnar-ip224@nung.edu.ua>
2026-04-26 19:50:48 +0300

    complete lab3

---
+++ Lab3/MathLibrary/MathFunctions.cs
@@ -0,0 +1,19 @@
+﻿namespace MathLibrary;
+
+public static class MathFunctions
+{
+    public static double Modulus(double a, double b)
+    {
+        return a % b;
+    }
+
+    public static double Power(double a, double b)
+    {
+        return Math.Pow(a, b);
+    }
+
+    public static double Sqrt(double a)
+    {
+        return Math.Sqrt(a);
+    }
+}
